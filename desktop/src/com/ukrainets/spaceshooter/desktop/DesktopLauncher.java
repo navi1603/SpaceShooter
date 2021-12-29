d7226901592bf52885851dd553747a09bc8fc383
warlock <ukrainec1603@gmail.com>
2021-12-30 00:33:47 +0300

    Add StarScape animation

--- desktop/src/com/ukrainets/spaceshooter/desktop/DesktopLauncher.java
+++ desktop/src/com/ukrainets/spaceshooter/desktop/DesktopLauncher.java
@@ -7,6 +7,7 @@ import com.ukrainets.spaceshooter.SpaceShooter;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setWindowSizeLimits(360,640,360,640);
 		new Lwjgl3Application(new SpaceShooter(), config);
 	}
 }

57740106d5e62dea313bf5abb0b33b538c79882a
warlock <ukrainec1603@gmail.com>
2022-01-10 01:10:25 +0300

    Add TextureAtlas, class Ship

--- core/src/com/ukrainets/spaceshooter/GameScreen.java
+++ core/src/com/ukrainets/spaceshooter/GameScreen.java
@@ -3,12 +3,13 @@ package com.ukrainets.spaceshooter;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
-public class GameScreen implements Screen {
+class GameScreen implements Screen {
 
     //screen
     private Camera camera;
@@ -16,35 +17,68 @@ public class GameScreen implements Screen {
 
     //graphics
     private SpriteBatch batch;
-    private Texture[] backgrounds;
+    private TextureAtlas textureAtlas;
+
+    private TextureRegion[] backgrounds;
+    private float backgroundHeight; //height of background in World units
+
+    private TextureRegion playerShipTextureRegion, playerShieldTextureRegion,
+            enemyShipTextureRegion, enemyShieldTextureRegion,
+            playerLaserTextureRegion, enemyLaserTextureRegion;
+
 
     //timing
-    private float[] backgroundOffsets = {0,0,0,0};
+    private float[] backgroundOffsets = {0, 0, 0, 0};
     private float backgroundMaxScrollingSpeed;
 
     //world parameters
     private final int WORLD_WIDTH = 72;
     private final int WORLD_HEIGHT = 128;
 
+    //game objects
+    private Ship playerShip;
+    private Ship enemyShip;
+
+
     GameScreen() {
+
         camera = new OrthographicCamera();
         viewport = new StretchViewport(WORLD_WIDTH, WORLD_HEIGHT, camera);
 
-        backgrounds = new Texture[4];
-        backgrounds[0] = new Texture("Starscape00.png");
-        backgrounds[1] = new Texture("Starscape01.png");
-        backgrounds[2] = new Texture("Starscape02.png");
-        backgrounds[3] = new Texture("Starscape03.png");
+        //set up the texture atlas
+        textureAtlas = new TextureAtlas("images.atlas");
 
-        backgroundMaxScrollingSpeed = (float) WORLD_HEIGHT / 4;
+        //setting up the background
+        backgrounds = new TextureRegion[4];
+        backgrounds[0] = textureAtlas.findRegion("Starscape00");
+        backgrounds[1] = textureAtlas.findRegion("Starscape01");
+        backgrounds[2] = textureAtlas.findRegion("Starscape02");
+        backgrounds[3] = textureAtlas.findRegion("Starscape03");
+
+        backgroundHeight = WORLD_HEIGHT * 2;
+        backgroundMaxScrollingSpeed = (float) (WORLD_HEIGHT) / 4;
+
+        //initialize texture regions
+        playerShipTextureRegion = textureAtlas.findRegion("playerShip2_blue");
+        enemyShipTextureRegion = textureAtlas.findRegion("enemyRed3");
+        playerShieldTextureRegion = textureAtlas.findRegion("shield2");
+        enemyShieldTextureRegion = textureAtlas.findRegion("shield1");
+        enemyShieldTextureRegion.flip(false, true);
+
+        playerLaserTextureRegion= textureAtlas.findRegion("laserBlue03");
+        enemyLaserTextureRegion= textureAtlas.findRegion("laserRed03");
+
+
+        //set up game objects
+        playerShip = new Ship(2, 3, 10, 10,
+                WORLD_WIDTH/2, WORLD_HEIGHT/4,
+                playerShipTextureRegion, playerShieldTextureRegion);
+        enemyShip = new Ship(2, 1, 10, 10,
+                WORLD_WIDTH/2, WORLD_HEIGHT*3/4,
+               enemyShipTextureRegion, enemyShieldTextureRegion);
+
 
         batch = new SpriteBatch();
-    }
-
-
-    @Override
-    public void show() {
-
     }
 
     @Override
@@ -54,25 +88,34 @@ public class GameScreen implements Screen {
         //scrolling background
         renderBackground(deltaTime);
 
+        //enemy ships
+        enemyShip.draw(batch);
+
+        //player ship
+        playerShip.draw(batch);
+
+        //lasers
+
+        //explosions
 
         batch.end();
     }
 
-    private void renderBackground (float deltaTime) {
+    private void renderBackground(float deltaTime) {
 
+        //update position of background images
         backgroundOffsets[0] += deltaTime * backgroundMaxScrollingSpeed / 8;
         backgroundOffsets[1] += deltaTime * backgroundMaxScrollingSpeed / 4;
         backgroundOffsets[2] += deltaTime * backgroundMaxScrollingSpeed / 2;
         backgroundOffsets[3] += deltaTime * backgroundMaxScrollingSpeed;
 
+        //draw each background layer
         for (int layer = 0; layer < backgroundOffsets.length; layer++) {
             if (backgroundOffsets[layer] > WORLD_HEIGHT) {
                 backgroundOffsets[layer] = 0;
             }
             batch.draw(backgrounds[layer], 0, -backgroundOffsets[layer],
-                    WORLD_WIDTH, WORLD_HEIGHT);
-            batch.draw(backgrounds[layer], 0, -backgroundOffsets[layer] + WORLD_HEIGHT,
-                    WORLD_WIDTH, WORLD_HEIGHT);
+                    WORLD_WIDTH, backgroundHeight);
         }
     }
 
@@ -98,7 +141,12 @@ public class GameScreen implements Screen {
     }
 
     @Override
-    public void dispose() {
+    public void show() {
 
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
     }
 }
